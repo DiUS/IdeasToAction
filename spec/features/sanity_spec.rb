29cab74149ee7c4a09faf198ec789a196ff87e89
Tal Rotbart <redbeard@gmail.com>
2013-01-31 15:08:52 +1100

    Added capybara (& webkit) and removed test unit.

---
+++ spec/features/sanity_spec.rb
@@ -0,0 +1,10 @@
+describe "End to End Sanity Check", :js => true do
+  
+  before :each do
+    visit '/assets/index.html'
+  end
+
+  it "should have a title" do
+    page.should have_selector("title", text: 'Actionman')
+  end
+end
