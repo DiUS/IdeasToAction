3200792ae6ec371945c7ef71e2f6f96a92665f08
azzamallow <azzamallow@gmail.com>
2013-02-04 17:03:36 +1100

    actions taken on an idea now coming from backend

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,4 +2,6 @@ class Action < ActiveRecord::Base
   attr_accessible :description
 
   belongs_to :idea
+
+  has_many :actions_taken
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,16 @@
 class Idea < ActiveRecord::Base
+  has_many :actions
+
   attr_accessible :body, :actions
 
-  has_many :actions
+  def as_json options = nil
+    super.merge(members_actioned_count: members_actioned.size)
+  end
+
+  private
+  def members_actioned
+    Idea.find_by_sql("select distinct at.member_id from " + 
+                      "ideas i, actions_taken at, actions a where " +
+                      "a.idea_id = i.id and at.action_id = a.id and i.id = #{self.id}")
+  end
 end
