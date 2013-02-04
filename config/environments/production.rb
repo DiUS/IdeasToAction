869f00e747b54f32d42c99e22bfcf2b593873bf4
azzamallow <azzamallow@gmail.com>
2013-02-04 13:20:22 +1100

    turned on compression for production, no mangling for now as angular doesnt play nice with it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,16 @@ Actionman::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  # Dont mangle, angular doesnt play nice
+  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
