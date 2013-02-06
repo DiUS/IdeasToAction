5c23e7af781501706d8368b006ee581f6283b94f
Tal Rotbart <redbeard@gmail.com>
2013-02-06 12:05:41 +1100

    Prototyped a member auto-login.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130205001845) do
+ActiveRecord::Schema.define(:version => 20130206003903) do
 
   create_table "actions", :force => true do |t|
     t.text     "description"
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(:version => 20130205001845) do
   end
 
   create_table "members", :force => true do |t|
-    t.string   "token"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "persistence_token"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
   end
 
   create_table "reactions", :force => true do |t|
