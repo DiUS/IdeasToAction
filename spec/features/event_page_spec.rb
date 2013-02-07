c9ca7b71e15153512fe241315ad568f57ebea840
azzamallow <azzamallow@gmail.com>
2013-02-08 10:11:29 +1100

    event page acceptance tests written

---
+++ spec/features/event_page_spec.rb
@@ -0,0 +1,33 @@
+require_relative 'collapsible_shared_examples'
+
+describe "Event page", js: true, acceptance: true do
+  
+  let(:event) { Event.find(1) }
+
+  before :each do
+    event.should_not be_nil
+    visit "/assets/index.html#/events/#{event.id}"
+  end
+
+  it "should have the event page visible" do
+    page.should have_content event.name
+
+    page.should have_content "Talks #{event.talks.size}"
+    page.should have_content "Ideas #{event.ideas.size}"
+  end
+
+  context "Talks collapsible" do
+    let(:title) { "Talks" }
+    let(:item_contents) { event.talks.map(&:title) }
+
+    it_should_behave_like "a collapsible"
+  end
+
+  context "Ideas collapsible" do
+    let(:title) { "Ideas" }
+    let(:item_contents) { event.ideas.map(&:body) }
+
+    it_should_behave_like "a collapsible"
+  end
+
+end
