ce96f1c2ca25207c00f9f312a6be0c58338fd8b6
azzamallow <azzamallow@gmail.com>
2013-02-07 11:46:04 +1100

    ideas can now be found for a specific event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ActiveRecord::Base
   attr_accessible :name, :talks
 
   has_many :talks
+  has_many :ideas, :through => :talks
 end
---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe EventsController do
+  def valid_attributes
+    { }
+  end
+
+  def valid_session
+    {}
+  end
+
+  describe "GET show" do
+    it "assigns the requested idea as @idea" do
+      event = Event.create! valid_attributes
+      get :show, {:id => event.to_param}, valid_session
+      assigns(:event).should eq(event)
+    end
+  end
+
+  describe 'GET ideas' do
+    it 'gets ideas' do
+      event_chain = mock :event
+      Event.should_receive(:find).with('1').and_return event_chain
+      event_chain.should_receive(:ideas)
+      get :ideas, {:id => 1, :format => 'json'}, valid_session
+    end
+  end
+
+end
