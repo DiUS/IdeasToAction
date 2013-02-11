8c24459e32a6de4dbc04926c7b937f2b6a6d340d
azzamallow <azzamallow@gmail.com>
2013-02-12 09:48:05 +1100

    idea view model updated to make specs happy

--- app/view_models/idea_view_model.rb
+++ app/view_models/idea_view_model.rb
@@ -5,14 +5,14 @@ class IdeaViewModel
     @idea_view_model = idea.as_json
 
     actions = idea.actions
-    @idea_view_model[:actions] = []
+    @idea_view_model['actions'] = []
 
     actions.each do |action|
-      @idea_view_model[:actions] << action.as_json
-      @idea_view_model[:actions].last[:member_action_taken] = action.actions_taken.from_member(params[:member])
+      @idea_view_model['actions'] << action.as_json
+      @idea_view_model['actions'].last['member_action_taken'] = action.actions_taken.from_member(params[:member])
     end
 
-    @idea_view_model[:reactions] = idea.reactions.as_json
+    @idea_view_model['reactions'] = idea.reactions.as_json
   end
 
   def as_json
