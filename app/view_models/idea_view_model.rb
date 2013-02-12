f2653318ccc3f0e308abdc306efe32c2183605ba
azzamallow <azzamallow@gmail.com>
2013-02-12 11:06:46 +1100

    when taking action, the date is then showed and the button goes away

--- app/view_models/idea_view_model.rb
+++ app/view_models/idea_view_model.rb
@@ -9,7 +9,9 @@ class IdeaViewModel
 
     actions.each do |action|
       @idea_view_model['actions'] << action.as_json
-      @idea_view_model['actions'].last['member_action_taken'] = action.actions_taken.from_member(params[:member])
+      member_action_taken = action.actions_taken.from_member(params[:member])
+      @idea_view_model['actions'].last['member_action_taken'] = member_action_taken.as_json
+      @idea_view_model['actions'].last['member_action_taken']['created_at'] = member_action_taken.created_at.strftime("%b %d %Y") unless member_action_taken.nil?
     end
 
     @idea_view_model['reactions'] = idea.reactions.as_json
