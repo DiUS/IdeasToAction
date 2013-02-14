1e2cfbb109798dd3fb7a8d3690d270fe98831cf5
azzamallow <azzamallow@gmail.com>
2013-02-14 14:41:42 +1100

    home page now displays random idea and action

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -8,4 +8,16 @@ describe "Home page", js: true, acceptance: true do
   it "should display welcome text that rattles the bones" do
     page.text.should =~ /(\d+) people; (\d+) ideas; (\d+) actions taken: - transforming ideas worth spreading into actions /
   end
+
+  it "should display a random idea" do
+    random_idea = page.find("#random-idea")
+    random_idea.should_not be_nil
+    random_idea.text.should_not be_empty
+  end
+
+  it "should display a random action" do
+    random_action = page.find("#random-action")
+    random_action.should_not be_nil
+    random_action.text.should_not be_empty
+  end
 end
