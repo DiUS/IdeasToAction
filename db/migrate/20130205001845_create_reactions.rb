25ab35ce0f47dc25f2a7c8281dbc340ad36f37a2
Tal Rotbart <redbeard@gmail.com>
2013-02-28 11:11:50 +1100

    Changed .string types to .text to avoid trimming of text.

--- db/migrate/20130205001845_create_reactions.rb
+++ db/migrate/20130205001845_create_reactions.rb
@@ -3,7 +3,7 @@ class CreateReactions < ActiveRecord::Migration
     create_table :reactions do |t|
       t.integer :member_id
       t.integer :idea_id
-      t.string  :text
+      t.text  :text
       t.timestamps
     end
   end
