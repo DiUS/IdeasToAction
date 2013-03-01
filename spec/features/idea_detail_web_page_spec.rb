807c459cfd4fe87a4d27f7378af098266b408dec
azzamallow <azzamallow@gmail.com>
2013-03-01 12:01:26 +1100

    added icon for android download app link

--- spec/features/idea_detail_web_page_spec.rb
+++ spec/features/idea_detail_web_page_spec.rb
@@ -8,13 +8,13 @@ describe "Idea detail web page", js: true, acceptance: true do
   end
 
   it "should have a link to open the app" do
-    link = page.find_link('Open in app')
+    link = page.find_link('Open in application')
     link['href'].should eql 'ideasintoaction://17'
   end
 
   it "should have a link to download the app" do
-    link = page.find_link('Download application')
-    link['href'].should eql 'http://play.google.com/store/apps/details?id=com.ted.actionman'
+    link = page.find('.download')
+    link['href'].should eql 'https://play.google.com/store/apps/details?id=com.ted.actionman'
   end
 
   it 'should display the idea' do
