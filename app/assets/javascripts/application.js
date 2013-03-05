10730853230b36f1cace1d3487c2114355dac1fb
azzamallow <azzamallow@gmail.com>
2013-03-06 09:23:14 +1100

    moved event resource into its own file, reduce the clutter in app.coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 //= require_tree ./snappy-swipe-nav/
 //= require ../app/app.coffee
 //= require_tree ../app/components
+//= require_tree ../app/resources
 //= require_tree ../app/controllers
 //= require_tree ../app/views
 //= require_tree .
