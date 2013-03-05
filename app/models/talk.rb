4c5478b85a9607e348ab4f41a5b66c6cd574fdd8
Tal Rotbart <redbeard@gmail.com>
2013-03-06 09:11:07 +1100

    Added explicit mapping to search indexing.

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -4,6 +4,17 @@ class Talk < ActiveRecord::Base
 
   index_name { "#{Rails.env}-#{table_name}" }
       
+  mapping do
+    indexes :id,              :index    => :not_analyzed
+    indexes :event_id,        :index    => :not_analyzed
+    indexes :created_at,      :type => 'date', :include_in_all => false
+    indexes :updated_at,      :type => 'date', :include_in_all => false
+    indexes :title,           :boost => 100
+    indexes :description
+    indexes :ted_talk_url,    :index    => :not_analyzed
+    indexes :hero_image_url,  :index    => :not_analyzed
+  end  
+      
   attr_accessible :description, :title, :ideas, :hero_image_url, :ted_talk_url
 
   has_many :ideas, through: :talk_to_idea_associations
