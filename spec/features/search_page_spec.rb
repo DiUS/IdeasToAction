06fa018d54d677f0fc70be2907595550a5d9ffbd
Tal Rotbart <redbeard@gmail.com>
2013-03-05 15:49:15 +1100

    Will test theory about cause of the acceptance tests failing.

--- spec/features/search_page_spec.rb
+++ spec/features/search_page_spec.rb
@@ -14,6 +14,7 @@ describe "Search page", js: true, acceptance: true do
     end
 
     it "should have results" do
+      sleep 1
       page.should have_selector(".result", text: "Examine your own body language")
       page.should have_selector(".result", text: "Body language affects how others see us, but it may also change how we see ourselves.")
     end
