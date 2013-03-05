38c336c21c6b675bb70fe6c1cbc6493035175be0
Tal Rotbart <redbeard@gmail.com>
2013-03-05 15:31:26 +1100

    Added basic end to end test for search page.

---
+++ spec/features/search_page_spec.rb
@@ -0,0 +1,21 @@
+describe "Search page", js: true, acceptance: true do
+  before do
+    visit "#/search"
+  end 
+
+  it "should have a search text field" do
+    page.should have_selector("#search-text", visible: true)
+  end
+
+  context "when typing a search query" do
+
+    before :each do 
+      fill_in "search-text", with: "body language"
+    end
+
+    it "should have results" do
+      page.should have_selector(".result", text: "Examine your own body language")
+      page.should have_selector(".result", text: "Body language affects how others see us, but it may also change how we see ourselves.")
+    end
+  end
+end
