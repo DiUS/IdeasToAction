5f0651b57a3ff5620aaefd0624e43af2fbef1e75
azzamallow <azzamallow@gmail.com>
2013-03-07 15:50:41 +1100

    fixed tests around going to home page from other pages

--- spec/features/idea_detail_page_spec.rb
+++ spec/features/idea_detail_page_spec.rb
@@ -60,7 +60,7 @@ describe "Idea detail page", js: true, acceptance: true do
   describe 'when wanting to navigate back to the home page' do
     before do
       page.should_not have_selector('#home')
-      page.find('.header .home-page').click
+      page.first('.header .touch-icon').click
     end
 
     it 'should navigate back to home' do
--- spec/features/member_page_spec.rb
+++ spec/features/member_page_spec.rb
@@ -50,7 +50,7 @@ describe "Member page", js: true, acceptance: true do
   describe 'when wanting to navigate back to the home page' do
     before do
       page.should_not have_selector('#home')
-      page.find('.header .home-page').click
+      page.first('.header .touch-icon').click
     end
 
     it 'should navigate back to home' do
