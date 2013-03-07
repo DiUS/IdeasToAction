cf55e523856ebf1be252702c19f21793a3a3e9a3
azzamallow <azzamallow@gmail.com>
2013-03-08 10:02:45 +1100

    getting a talk now includes associated ideas in its payload

--- spec/controllers/talks_controller_spec.rb
+++ spec/controllers/talks_controller_spec.rb
@@ -1,18 +1,25 @@
 require 'spec_helper'
 
 describe TalksController do
-  describe 'GET index' do
-    let(:event) { Event.find(1) }
-    let(:talk) { event.talks.first }
+  let(:event) { Event.find(1) }
+  let(:talk) { event.talks.first }
 
+  describe 'GET index' do
     let(:other_event) { Event.find(2) }
     let(:other_talk) { other_event.talks.first }
 
     it "should nest in events" do
-      get :index, { :event_id => 1, :format => 'json'}
+      get :index, { :event_id => 1, :format => :json }
 
       assigns(:talks).should include(talk)
       assigns(:talks).should_not include(other_talk)
+    end
+  end
+
+  describe 'GET show' do
+    it 'should do its thing' do
+      get :show, { :id => talk.id, :format => :json }
+      assigns(:talk).should eql talk
     end
   end
 end
