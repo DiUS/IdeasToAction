ef00235ac35f1a06315ebec37ce455597f48d1cd
alexs <aserdyuk@dius.com.au>
2013-03-12 16:30:48 +1100

    VCR config.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,6 +2,7 @@ VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join("spec", "vcr")
   c.hook_into :fakeweb
   c.ignore_localhost = true
+  c.default_cassette_options = { :allow_playback_repeats => true }
 end
 
 RSpec.configure do |c|
