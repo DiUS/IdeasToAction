0a9abcec7d5def00e9de06a9087217dbeaaff342
azzamallow <azzamallow@gmail.com>
2013-03-13 14:26:31 +1100

    vcr now ignoring bitly url param

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,5 +2,8 @@ VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join("spec", "vcr")
   c.hook_into :fakeweb
   c.ignore_localhost = true
-  c.default_cassette_options = { :allow_playback_repeats => true }
+  c.default_cassette_options = { 
+    :allow_playback_repeats => true, 
+    :match_requests_on => [:method, VCR.request_matchers.uri_without_param(:longUrl)]
+  }
 end
