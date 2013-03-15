6a51b99e7f2a9d1a906b2a09f7a6930fa9ea6993
alexs <aserdyuk@dius.com.au>
2013-03-15 16:17:03 +1100

    Removed matching for ides param.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,6 +4,6 @@ VCR.configure do |c|
   c.ignore_localhost = true
   c.default_cassette_options = { 
     :allow_playback_repeats => true, 
-    :match_requests_on => [:method, VCR.request_matchers.uri_without_param(:longUrl)]
+    :match_requests_on => [:method, VCR.request_matchers.uri_without_params(:longUrl, :ideas)]
   }
 end
