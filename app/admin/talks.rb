8b3a801471b5c6026abd4fd74a60c0781d178b83
alexs <aserdyuk@dius.com.au>
2013-03-20 09:41:54 +1100

    Created ActiveAdmin interface for events, talks and ideas with dashboard.

---
+++ app/admin/talks.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Talk do
+  config.per_page = 10
+
+  index do
+    column :title
+    column :description, :sortable => :description do |talk|
+      truncate talk.description, :length => 100
+    end
+    column "Event" do |talk|
+      link_to talk.event_name, admin_event_path(talk.event)
+    end
+    column :featured
+    column :created_at
+    column :updated_at
+    default_actions
+  end
+end
