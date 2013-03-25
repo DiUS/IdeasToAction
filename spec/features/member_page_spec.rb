1cd57f598ab41a99462754d44b0ec4d8dc96d7a4
azzamallow <azzamallow@gmail.com>
2013-03-26 09:51:04 +1100

    member acceptance re-introduced

---
+++ spec/features/member_page_spec.rb
@@ -0,0 +1,34 @@
+require_relative 'collapsible_shared_examples'
+
+describe "Member page", js: true, acceptance: true do
+  self.use_transactional_fixtures = false
+
+  before do
+    Capybara.reset_sessions!
+    visit '/heartbeat/beat.json'
+  end
+
+  let(:heartbeat) { JSON.parse(page.text) }
+  let(:member_id) { heartbeat["member_id"] }
+  let(:member) { Member.find(member_id) }
+
+  before do
+    member.should_not be_nil
+    @interaction = Interaction.create! idea_action: IdeaAction.first, member: member, reaction_text: 'i think you have come up with something big here'
+    member.interactions.size.should > 0
+    visit "#/member"
+  end
+
+  after :each do
+    member.destroy
+    @interaction.destroy
+  end
+
+  context "My actions collapsible" do
+    let(:title) { "My actions" }
+    let(:starts_as_collapsed?) { false }
+    let(:item_contents) { member.interactions.map(&:idea_action).map(&:description) }
+
+    it_should_behave_like "a collapsible"
+  end
+end
