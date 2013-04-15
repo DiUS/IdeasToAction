cf3588e692d41e64e9ca15ff3f5f2cfcabca228c
azzamallow <azzamallow@gmail.com>
2013-04-15 10:42:26 +1000

    event view updated to pass an array of ideas rather than an array of events

--- app/models/view_models/event_view.rb
+++ app/models/view_models/event_view.rb
@@ -8,10 +8,10 @@ class EventView
     self.featured = random_featured
     already_selected.concat random_featured
 
-    random_recent = Event.recent.excluding_events(already_selected).random(2)
+    random_recent = Event.recent.excluding_events(already_selected.collect(&:id)).random(2)
     self.recent = random_recent
     already_selected.concat random_recent
 
-    self.popular =  Event.popular.excluding_events(already_selected).random(2)
+    self.popular =  Event.popular.excluding_events(already_selected.collect(&:id)).random(2)
   end
 end
