5ba6b6839a0075c84da759551c6f6a2be58315de
alexs333 <aserdyuk@dius.com.au>
2014-09-18 16:21:41 +1000

    Updated production references.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :domain,      "production.actionman.zerobot.io"
+set :domain,      '54.79.75.3'
 set :rails_env,   "production"
 set :app_env,     "production"
 set :branch,      ENV["PIPELINE_VERSION"] || 'master'
