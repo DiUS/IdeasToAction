377467cf862c8ebb7e3daf4030319ded25dc63ae
alexs333 <aserdyuk@dius.com.au>
2014-10-02 11:49:49 +1000

    Removed comments and changed top nav label.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Actionman Admin"
+  config.site_title = 'TEDxUltimo StartSomething Admin'
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -109,7 +109,7 @@ ActiveAdmin.setup do |config|
   #   config.namespace :without_comments do |without_comments|
   #     without_comments.allow_comments = false
   #   end
-
+	config.allow_comments = false
 
   # == Batch Actions
   #
