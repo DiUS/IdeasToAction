2f5bf8fb42624dc33cee26a124fb54e1b16479d3
thoran <code@thoran.com>
2014-10-02 16:51:36 +1000

    + Mailer specs with some alteration to Mailer itself.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,21 +1,26 @@
 class Mailer < ActionMailer::Base
 
-  def remind(*members)
-    members = Member.remindable_actions if members.flatten!.blank?
-    members.each do |member|
-      if member.remindable?
-        begin
-          @actions = member.remindable_actions
-          to = member.email
-          subject = "TEDx Reminder"
-          mail(to: to, subject: subject)
-          IdeaAction.reminded(@actions)
-        rescue *SMTP_ERRORS => e
-          Rails.logger.error("Attempting to send an email to #{to} with subject #{subject} produced an error.")
-          Rails.logger.error(e)
-        end
+  default from: "no-reply+#{Rails.env}@tedx.com.au"
+
+  def remind(member)
+    if member.remindable?
+      begin
+        @actions = member.remindable_actions
+        to = member.email
+        subject = "TEDx Reminder"
+        mailer = mail(to: to, subject: subject)
+        IdeaAction.reminded(@actions)
+        mailer
+      rescue *SMTP_ERRORS => e
+        Rails.logger.error("Attempting to send an email to #{to} with subject #{subject} produced an error.")
+        Rails.logger.error(e)
       end
     end
+  end
+
+  def reminders(*members)
+    members = Member.remindable_actions if members.flatten!.blank?
+    members.each{|member| remind(member)}
   end
 
 end
---
+++ spec/mailers/mailer_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Mailer do
+
+  describe "remind" do
+    let(:idea_action){double(IdeaAction, description: 'description', update_attribute: true)}
+    let(:remindable_actions){[idea_action]}
+    let(:member){double(Member, email: 'to@example.org', remindable?: true, remindable_actions: remindable_actions)}
+    let(:mail){Mailer.remind(member)}
+    subject{mail}
+
+    its(:subject){should match('TEDx Reminder')}
+    its(:to){should eq(['to@example.org'])}
+    its(:from){should eq(["no-reply+test@tedx.com.au"])}
+
+    describe "#body" do
+      subject{mail.body.encoded}
+      it{should match('OMG, the target date for the following action')}
+    end
+  end
+
+end
