090acc855464c988af164a1a14e75568d322fdd7
thoran <code@thoran.com>
2014-10-06 13:35:48 +1100

    I forgot that I reorder them according to how they appear on the home page.

--- app/controllers/counts_controller.rb
+++ app/controllers/counts_controller.rb
@@ -3,9 +3,9 @@ class CountsController < ApplicationController
   def index
     render json: {
       ideas: Idea.total,
-      idea_actions: IdeaAction.total
+      idea_actions: IdeaAction.total,
       talks: Talk.total,
-      events: Event.total,
+      events: Event.total
     }
   end
 end
