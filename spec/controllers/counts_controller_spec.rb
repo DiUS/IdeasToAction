781453b67660ca3a842e5bd7d1145566694e8d75
thoran <code@thoran.com>
2014-10-06 14:01:20 +1100

    The counts_controller_spec also broke because of the reordering of the counts/totals.

--- spec/controllers/counts_controller_spec.rb
+++ spec/controllers/counts_controller_spec.rb
@@ -4,15 +4,16 @@ describe CountsController do
 
   describe "GET index" do
     before do
-      Event.should_receive(:count).and_return(5)
-      Idea.should_receive(:count).and_return(6)
-      IdeaAction.should_receive(:count).and_return(7)
-      Talk.should_receive(:count).and_return(8)
+      Idea.should_receive(:total).and_return(6)
+      IdeaAction.should_receive(:total).and_return(7)
+      Talk.should_receive(:total).and_return(8)
+      Event.should_receive(:total).and_return(5)
     end
 
     it "returns counts for member, idea and actions taken" do
       get :index
-      response.body.should == { :events => 5, :talks => 8, :ideas => 6, :idea_actions => 7 }.to_json
+      expect(response.body).to eq({ideas: 6, idea_actions: 7, talks: 8, events: 5}.to_json)
     end
   end
+
 end
