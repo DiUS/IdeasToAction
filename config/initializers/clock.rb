62ecc302c397f57e03f369ca3a354bf5785e1031
thoran <code@thoran.com>
2014-10-06 18:38:08 +1100

    Using a more explicit task name for the Clockwork job name.

--- config/initializers/clock.rb
+++ config/initializers/clock.rb
@@ -1,7 +1,7 @@
 require_relative '../environment'
 
 module Clockwork
-  every(1.day, 'remind', at: '00:00') do
+  every(1.day, 'Mailer.reminders', at: '00:00') do
     Mailer.delay.reminders
   end
 end
