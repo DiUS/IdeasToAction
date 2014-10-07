aadb6ff275b3bf070657c66c561fcd1da66e996a
thoran <code@thoran.com>
2014-10-07 11:40:53 +1100

    Rails.application.config brilliantly doesn't trap the non-existence of an option and return like, say, a Hash would, so I have to check for whether it will respond to an option first which is a bit dumb.  Is there any reason why nil isn't a reasonable response to a configuration option which is not present?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
   def env_has_analytics?
-    !!Rails.application.config.analytics_tracking_key
+    Rails.application.config.respond_to?(:analytics_tracking_key) &&
+      !!Rails.application.config.analytics_tracking_key
   end
 
 end
