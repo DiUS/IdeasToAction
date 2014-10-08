65bb9f6d425483d6761efdcea7342be77a09e87a
thoran <code@thoran.com>
2014-10-08 11:48:25 +1100

    In order to compile the production assets the app must switch to production, which means that the production database should exist and needs to be specified in the Environmentfile on a per environment basis.  Also removed the Environmentfile.parse and Environmentfile.[] methods, since I now have conditionals in there.  And finally tidied up the database.yml file which had a duplicate environment reference in there.

--- lib/Environmentfile.rb
+++ lib/Environmentfile.rb
@@ -10,19 +10,6 @@ class Environmentfile
       instance_eval(read)
     end
 
-    def parse
-      envar_lines = read.split("\n").reject{|line| line =~/^#/}.reject{|line| line == ''}
-      envar_lines.inject({}) do |envars, envar_line|
-        envar, enval = envar_line.captures(/ENV\['(.+)'\].*=.*['"](.+)['"]/)
-        envars[envar] = enval
-        envars
-      end
-    end
-
-    def [](envar)
-      parse[envar]
-    end
-
   end # class << self
 
 end
