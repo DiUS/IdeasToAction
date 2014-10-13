5a69583b70c661ed0a07fa5132f0f301db6a619d
thoran <code@thoran.com>
2014-10-13 18:37:42 +1100

    /admin_authenticated/authenticated/ and did some tidying in the general vicinity, mostly dehashrocketification.

--- app/controllers/member_sessions_controller.rb
+++ app/controllers/member_sessions_controller.rb
@@ -1,7 +1,8 @@
 class MemberSessionsController < ApplicationController
-  layout "web_login"
 
-	before_filter :create_member_if_new, :only => :create
+  layout 'web_login'
+
+  before_filter :create_member_if_new, only: :create
 
   def new
     @member_session = MemberSession.new
@@ -12,43 +13,43 @@ class MemberSessionsController < ApplicationController
 
     respond_to do |format|
       if @member_session.save
-        format.html { redirect_to admin_root_path }
-        format.json { render json: { success: true, id: current_member.id, email: current_member.email} }
+        format.html {redirect_to admin_root_path}
+        format.json {render json: {success: true, id: current_member.id, email: current_member.email}}
       else
-        format.html { render 'new' }
-        format.json { render json: { error: 'Email address is invalid' }, :status => :unprocessable_entity }
+        format.html {render 'new'}
+        format.json {render json: {error: 'Email address is invalid'}, status: :unprocessable_entity}
       end
     end
   end
-  
+
   def logout
     @member_session = MemberSession.find
 
     respond_to do |format|
       if @member_session.destroy
-        format.html { redirect_to new_member_session_path, :notice => "You have logged out successfully." }
-        format.json { render json: { success: "true" } }
+        format.html {redirect_to new_member_session_path, notice: 'You have logged out successfully.'}
+        format.json {render json: {success: 'true'}}
       else
-        format.json { render json: { errors: @member_session.errors }, :status => :unprocessable_entity }
+        format.json {render json: {errors: @member_session.errors}, status: :unprocessable_entity}
         format.html do
-          redirect_to admin_root_path, :error => "Logout failed, please try again."
+          redirect_to admin_root_path, error: 'Logout failed, please try again.'
         end
       end
     end
   end
 
   def check
-    member_check =  { admin_authenticated: current_member && current_member.email.present? }
+    member_check = {authenticated: current_member && current_member.email.present?}
     member_check[:id] = current_member.id if current_member
     member_check[:email] = current_member.email if current_member
     render json: member_check
-	end
+  end
 
-	private
+  private
 
-	def create_member_if_new
-		if Member.where(email: params[:member_session][:email]).empty?
-			Member.create email: params[:member_session][:email], password: params[:member_session][:password]
-		end
-	end
+  def create_member_if_new
+    if Member.where(email: params[:member_session][:email]).empty?
+      Member.create(email: params[:member_session][:email], password: params[:member_session][:password])
+    end
+  end
 end
--- spec/controllers/member_sessions_controller_spec.rb
+++ spec/controllers/member_sessions_controller_spec.rb
@@ -1,25 +1,26 @@
 require 'spec_helper'
 
 describe MemberSessionsController do
+
   before :each do
     activate_authlogic
-    @admin_member = Member.create!(:email => 'ted@x.com', :password => 'admin')
+    @admin_member = Member.create!(email: 'ted@x.com', password: 'admin')
   end
 
-  describe  "when round trip" do
+  describe "when round trip" do
     it "should login a user" do
-      post :create, :member_session => { :email => 'ted@x.com', :password => 'admin' }
+      post :create, member_session: {email: 'ted@x.com', password: 'admin'}
       assigns(:member_session).record.should eql(@admin_member)
       response.should be_redirect
     end
 
     it "should not login an invalid user" do
-      post :create, :member_session => { :email => 'invalid_email', :password => 'invalid_password', :format => 'json' }
+      post :create, member_session: {email: 'invalid_email', password: 'invalid_password', format: 'json' }
       assigns(:member_session).record.should_not eql(@admin_member)
     end
 
     it "should logout the user" do
-      delete :logout, {:id => @admin_member.id}
+      delete :logout, {id: @admin_member.id}
       response.should be_redirect
     end
   end
@@ -27,29 +28,28 @@ describe MemberSessionsController do
   describe "when json/ajax" do
     it "should report user status" do
       xhr :get, :check
-
-      ActiveSupport::JSON.decode(response.body)["admin_authenticated"].should be_true
-      ActiveSupport::JSON.decode(response.body)["id"].should eql(@admin_member.id)
-      ActiveSupport::JSON.decode(response.body)["email"].should eql(@admin_member.email)
-
+      ActiveSupport::JSON.decode(response.body)['authenticated'].should be_true
+      ActiveSupport::JSON.decode(response.body)['id'].should eql(@admin_member.id)
+      ActiveSupport::JSON.decode(response.body)['email'].should eql(@admin_member.email)
       response.should be_success
     end
 
     it "should login a user" do
-      xhr :post, :create, :member_session => { :email => 'ted@x.com', :password => 'admin'}, :format => :json
+      xhr :post, :create, member_session: {email: 'ted@x.com', password: 'admin'}, format: :json
       assigns(:member_session).record.should eql(@admin_member)
       response.should be_success
     end
 
     it "should not login an invalid user" do
-      xhr :post, :create, :member_session => { :email => 'invalid_email', :password => 'invalid_password' }, :format => :json
+      xhr :post, :create, member_session: {email: 'invalid_email', password: 'invalid_password'}, format: :json
       assigns(:member_session).record.should_not eql(@admin_member)
     end
 
     it "should logout the user" do
-      xhr :delete, :logout, {:id => @admin_member.id, :format => :json}
+      xhr :delete, :logout, {id: @admin_member.id, format: :json}
       ActiveSupport::JSON.decode(response.body)["success"].should be_true
       response.should be_success
     end
   end
+
 end
