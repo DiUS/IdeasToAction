5a68cddcfb2698691765533a781e7f5246df68d0
thoran <code@thoran.com>
2014-10-13 18:09:44 +1100

    Trying to get spec coverage up, but this one hasn't done much good...

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -1,21 +1,39 @@
 require 'spec_helper'
 
 describe Mailer do
+  let(:idea_action){double(IdeaAction, description: 'description', update_attribute: true)}
+  let(:remindable_actions){[idea_action]}
+  let(:mail){Mailer.remind(member)}
+  subject{mail}
 
-  describe ".remind" do
-    let(:idea_action){double(IdeaAction, description: 'description', update_attribute: true)}
-    let(:remindable_actions){[idea_action]}
+  context "member.remindable? == true" do
     let(:member){double(Member, email: 'to@example.org', remindable?: true, remindable_actions: remindable_actions)}
-    let(:mail){Mailer.remind(member)}
-    subject{mail}
 
-    its(:subject){should match('TEDx Reminder')}
-    its(:to){should eq(['to@example.org'])}
-    its(:from){should eq(["no-reply+test@tedxultimo.com"])}
+    describe "self.remind" do
+      its(:subject){should match('TEDx Reminder')}
+      its(:to){should eq(['to@example.org'])}
+      its(:from){should eq(["no-reply+test@tedxultimo.com"])}
 
-    describe "#body" do
-      subject{mail.body.encoded}
-      it{should match('OMG, the target date for the following action')}
+      describe "#body" do
+        subject{mail.body.encoded}
+        it{should match('OMG, the target date for the following action')}
+      end
+    end
+
+    describe "self.reminders" do
+    end
+  end
+
+  context "member.remindable? == false" do
+    let(:member){double(Member, email: 'to@example.org', remindable?: false, remindable_actions: remindable_actions)}
+
+    describe "self.remind" do
+      it "must be null mail object" do
+        expect(subject.class).to be ActionMailer::Base::NullMail
+      end
+    end
+
+    describe "self.reminders" do
     end
   end
 
