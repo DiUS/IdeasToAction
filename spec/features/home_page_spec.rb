429ffdd270c0c38f553a91095ec49bca3f301574
alexs333 <aserdyuk@dius.com.au>
2014-10-13 11:18:30 +1100

    Pluralization handling for the home page (Ideas, Talks and Event counts).

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,4 +1,4 @@
-describe "Home page", js: true, acceptance: true do
+describe "Home page", js: true do
   self.use_transactional_fixtures = false
 
   before do
@@ -29,5 +29,58 @@ describe "Home page", js: true, acceptance: true do
     find('input.btn').click()
     sleep 1
     page.current_url.should match /#\/found\?query_text=stuff/
-  end
+	end
+
+	describe 'pluralization' do
+		context 'of events' do
+			it 'should be singular when 1 event' do
+				Event.stub(:size).and_return(1)
+
+				visit '/#/home'
+				expect(page).to have_content('Event')
+				expect(page).not_to have_content('Events')
+			end
+
+			it 'should be plural when 10 events' do
+				Event.stub(:size).and_return(10)
+
+				visit '/#/home'
+				expect(page).to have_content('Events')
+			end
+		end
+
+		context 'of talks' do
+			it 'should be singular when 1 talk' do
+				Talk.stub(:size).and_return(1)
+
+				visit '/#/home'
+				expect(page).to have_content('Talk')
+				expect(page).not_to have_content('Talks')
+			end
+
+			it 'should be plural when 10 talks' do
+				Talk.stub(:size).and_return(10)
+
+				visit '/#/home'
+				expect(page).to have_content('Talk')
+			end
+		end
+
+		context 'of ideas' do
+			it 'should should be singular when 1 idea' do
+				Idea.stub(:size).and_return(1)
+
+				visit '/#/home'
+				expect(page).to have_content('Idea')
+				expect(page).not_to have_content('Ideas')
+			end
+
+			it 'should be plural when 10 ideas' do
+				Idea.stub(:size).and_return(1)
+
+				visit '/#/home'
+				expect(page).to have_content('Ideas')
+			end
+		end
+	end
 end
