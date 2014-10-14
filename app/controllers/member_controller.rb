de14f682fc925172899257545ccc4413af4d6284
thoran <code@thoran.com>
2014-10-14 12:05:17 +1100

    Removal of tabs.

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -3,7 +3,7 @@ class MemberController < ApplicationController
 
   respond_to :json
 
-	def idea_actions
-		render json: current_member.idea_actions.incomplete_first.map{|a| a.attributes.merge(completable: a.completable?(current_member))}
-	end
+  def idea_actions
+    render json: current_member.idea_actions.incomplete_first.map{|a| a.attributes.merge(completable: a.completable?(current_member))}
+  end
 end
