86dd2c4f6883329edb22e977500ccb8d9cc79563
thoran <code@thoran.com>
2014-10-17 15:39:37 +1100

    /tabs/spaces/

--- spec/features/login_page_spec.rb
+++ spec/features/login_page_spec.rb
@@ -1,28 +1,28 @@
 describe 'Login page', js: true, acceptance: true do
-	before do
-		visit '/#/login'
-	end
+  before do
+    visit '/#/login'
+  end
 
-	describe 'page content' do
-		it 'should have a welcome message' do
-			expect(page).to have_content('Hi, welcome to TEDxUltimo')
-		end
+  describe 'page content' do
+    it 'should have a welcome message' do
+      expect(page).to have_content('Hello and welcome to TEDxUltimo')
+    end
 
-		it 'should have an email field' do
-			expect(page).to have_field('email')
-		end
-	end
-	
-	describe 'logging in' do
-		let(:email) { 'user@example.com' }
+    it 'should have an email field' do
+      expect(page).to have_field('email')
+    end
+  end
+  
+  describe 'logging in' do
+    let(:email) { 'user@example.com' }
 
-		before do
-			fill_in 'email', with: email
-		  find('#login-submit').click
-		end
+    before do
+      fill_in 'email', with: email
+      find('#login-submit').click
+    end
 
-		it 'should naviagate to home page' do
-			expect(page).to have_content('the ultimate place to start something')
-		end
-	end
+    it 'should naviagate to home page' do
+      expect(page).to have_content('The Ultimate Place to Start Something')
+    end
+  end
 end
