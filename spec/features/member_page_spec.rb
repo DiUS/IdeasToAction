facd708f3393ce46b58fc2bba8ea1eb1a5b9b498
thoran <code@thoran.com>
2014-10-17 15:40:43 +1100

    Member page acceptance test partially sorted.

--- spec/features/member_page_spec.rb
+++ spec/features/member_page_spec.rb
@@ -4,20 +4,11 @@ describe "Member page", js: true, acceptance: true do
   self.use_transactional_fixtures = false
 
   before do
-    Capybara.reset_sessions!
-    visit '/heartbeat/beat.json'
-  end
-
-  let(:heartbeat) { JSON.parse(page.text) }
-  let(:member_id) { heartbeat["member_id"] }
-  let(:member) { Member.find(member_id) }
-
-  before do
-    member.should_not be_nil
     visit "#/member"
   end
 
-  after :each do
-    member.destroy
-  end
+  # it "should have a list of actions" do
+  #   expect(page).to have_selector()
+  # end
+
 end
