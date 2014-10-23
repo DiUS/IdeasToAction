49755dc50e943dd7123309c2eb201fef741477ff
alexs333 <aserdyuk@dius.com.au>
2014-10-23 11:19:21 +1100

    Cleaned up spec helpers.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,7 +1,4 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start unless
-
-ENV["RAILS_ENV"] ||= 'test'
+ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -9,8 +6,8 @@ require 'rspec/autorun'
 
 require 'capybara/rails'
 require 'capybara/rspec'
-require "authlogic/test_case"
-require "rack_session_access/capybara"
+require 'authlogic/test_case'
+require 'rack_session_access/capybara'
 require 'webmock/rspec'
 require 'database_cleaner'
 require 'capybara/poltergeist'
@@ -25,32 +22,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 Capybara.javascript_driver = :poltergeist
 
 RSpec.configure do |config|
-	# ## Mock Framework
-	#
-	# If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-	#
-	# config.mock_with :mocha
-	# config.mock_with :flexmock
-	# config.mock_with :rr
-
-	# Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-	config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-	# If you're not using ActiveRecord, or you'd prefer not to run each of your
-	# examples within a transaction, remove the following line or assign false
-	# instead of true.
 	config.use_transactional_fixtures = true
-
-	# If true, the base class of anonymous controllers will be inferred
-	# automatically. This will be the default behavior in future versions of
-	# rspec-rails.
 	config.infer_base_class_for_anonymous_controllers = false
-
-	# Run specs in random order to surface order dependencies. If you find an
-	# order dependency and want to debug it, you can fix the order by providing
-	# the seed, which is printed after each run.
-	#     --seed 1234
-	config.order = "random"
+	config.order = 'random'
 
 	config.before :suite do
 		DatabaseCleaner.strategy = :transaction
